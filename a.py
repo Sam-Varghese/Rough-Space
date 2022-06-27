3d4176aa5f360ff22ade2cbe16875835f1c7fb7c
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-06-27 18:42:25 +0530

    Added report

---
+++ a.py
@@ -0,0 +1,9 @@
+import pywhatkit
+
+message = """
+*Happy Birthday Angel* 🎂🎂🎂
+
+https://sam-varghese.github.io/Rough-Space/
+"""
+
+pywhatkit.sendwhatmsg("+917828515205", "Hi", 23, 16, 00, True, 2)
